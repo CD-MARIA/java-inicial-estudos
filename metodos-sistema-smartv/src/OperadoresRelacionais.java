3f46a87d4b535a999fcdf3d8306dd938be6fa964
CD-MARIA <107446686+CD-MARIA@users.noreply.github.com>
2022-10-14 01:24:46 -0300

    Arquivo criado para testes de op relacionais

---
+++ metodos-sistema-smartv/src/OperadoresRelacionais.java
@@ -0,0 +1,45 @@
+
+/** 
+ * Arquivo de exemplo para o exercicio de operadores relacionais.
+ * 
+ */
+
+public class OperadoresRelacionais {
+    
+    public static void main(String[] args){
+
+        int i1 = 10;
+        int i2 = 20;
+        float f2 = 4.5f;
+        float f3 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 ="Fulano";
+        String s3 = "Cicrano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1597L;
+        long l2 = 899L;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("i1 == i2" + (i1 == i2));
+        System.out.println("i1 != i2" +(i1 != i2));
+        System.out.println("i1 > i2" + (i1 > i2));
+        System.out.println();
+        
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+}
